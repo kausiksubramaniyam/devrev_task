b189c434cc3317539057d1fe1d2ce33d3ed09a98
kausiksg <kausik.sg@zohocorp.com>
2023-05-31 00:00:53 +0530

    Getters and setters for Tickets

--- Ticket.java
+++ Ticket.java
@@ -5,4 +5,44 @@ public class Ticket {
     private Integer ticketCost;
 
     private Integer seatNumber;
+
+    public Integer getTicketId() {
+        return this.ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Integer getFlightNumber() {
+        return this.flightNumber;
+    }
+
+    public void setFlightNumber(Integer flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getTicketCost() {
+        return this.ticketCost;
+    }
+
+    public void setTicketCost(Integer ticketCost) {
+        this.ticketCost = ticketCost;
+    }
+
+    public Integer getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setSeatNumber(Integer seatNumber) {
+        this.seatNumber = seatNumber;
+    }
 }
