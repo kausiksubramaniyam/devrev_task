b3a690a206e1dd089130163b8e02b992d6bc1918
kausiksg <kausik.sg@zohocorp.com>
2023-05-31 00:34:37 +0530

    Date based flight search methods implemented

--- FlightsCRUD.java
+++ FlightsCRUD.java
@@ -3,6 +3,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,6 +67,74 @@ public class FlightsCRUD {
         return flights;
     }
 
+    public void searchFlightsByDate(LocalDateTime searchdate) throws SQLException {
+        String query = "SELECT * FROM Flights WHERE nextDeparture = ?";
+        PreparedStatement stmt = connection.prepareStatement(query);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = searchdate.format(formatter);
+
+        stmt.setString(1, formattedDateTime);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            int flightNumber = rs.getInt("flightNumber");
+            String flightModel = rs.getString("flightModel");
+            String airlineName = rs.getString("airlineName");
+            String fromDestination = rs.getString("fromDestination");
+            String toDestination = rs.getString("toDestination");
+            LocalDateTime nextDeparture = rs.getTimestamp("nextDeparture").toLocalDateTime();
+            int seatCount = rs.getInt("seatCount");
+
+            System.out.println("Flight Number: " + flightNumber);
+            System.out.println("Flight Model: " + flightModel);
+            System.out.println("Airline Name: " + airlineName);
+            System.out.println("From Destination: " + fromDestination);
+            System.out.println("To Destination: " + toDestination);
+            System.out.println("Next Departure: " + nextDeparture);
+            System.out.println("Seat Count: " + seatCount);
+            System.out.println("-----------------------------");
+        }
+    }
+
+    public void searchFlightsBetweenStartAndEndDate(LocalDateTime startDate, LocalDateTime endDate) throws SQLException {
+        try {
+            String sql = "SELECT * FROM Flights WHERE nextDeparture >= ? AND nextDeparture <= ?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedStartDateTime = startDate.format(formatter);
+            String formattedEndDateTime = endDate.format(formatter);
+
+            stmt.setString(1, formattedStartDateTime);
+            stmt.setString(2, formattedEndDateTime);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int flightNumber = rs.getInt("flightNumber");
+                String flightModel = rs.getString("flightModel");
+                String airlineName = rs.getString("airlineName");
+                String fromDestination = rs.getString("fromDestination");
+                String toDestination = rs.getString("toDestination");
+                LocalDateTime nextDeparture = rs.getTimestamp("nextDeparture").toLocalDateTime();
+                int seatCount = rs.getInt("seatCount");
+
+                System.out.println("Flight Number: " + flightNumber);
+                System.out.println("Flight Model: " + flightModel);
+                System.out.println("Airline Name: " + airlineName);
+                System.out.println("From Destination: " + fromDestination);
+                System.out.println("To Destination: " + toDestination);
+                System.out.println("Next Departure: " + nextDeparture);
+                System.out.println("Seat Count: " + seatCount);
+                System.out.println("-----------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     private Flights createFlightFromResultSet(ResultSet resultSet) throws SQLException {
         Flights flight = new Flights();
         flight.setFlightNumber(resultSet.getInt("flightNumber"));
